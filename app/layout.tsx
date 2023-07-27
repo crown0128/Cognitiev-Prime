0087f44bd4ee4c3b67ec99abe3205392c962ebaa
crown0128 <happycrown0128@gmail.com>
2023-07-27 23:34:20 +0530

    Update layout.tsx Name update Beta

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { getClientConfig } from "./config/client";
 import { type Metadata } from 'next';
 
 export const metadata: Metadata = {
-  title: "Cognitiev PRO",
+  title: "Cognitiev PRO Beta",
   description: "Your Ultimate AI Sidekick.",
   viewport: {
     width: "device-width",
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
     { media: "(prefers-color-scheme: dark)", color: "#151515" },
   ],
   appleWebApp: {
-    title: "Cognitiev PRO",
+    title: "Cognitiev PRO Beta",
     statusBarStyle: "default",
   },
 };
