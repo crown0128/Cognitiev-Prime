2b7634dd59aae2e85550e3885f7b18e14a582661
crown0128 <happycrown0128@gmail.com>
2023-08-13 00:27:10 +0530

    Update sidebar.tsx
    Sidebar Updated

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -136,13 +136,13 @@ export function SideBar(props: { className?: string }) {
           onClick={() => navigate(Path.NewChat, { state: { fromHome: true } })}
           shadow
         />
-        <IconButton
+        {/* <IconButton
           icon={<PluginIcon />}
           text={shouldNarrow ? undefined : Locale.Plugin.Name}
           className={styles["sidebar-bar-button"]}
           onClick={() => showToast(Locale.WIP)}
           shadow
-        />
+        />*/}
       </div>
 
       <div
